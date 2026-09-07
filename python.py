07f911cb780386f3be9e2726909e1d0331658d7b
testumate <shanzeezmalik@gmail.com>
2026-09-06 22:32:41 -0400

    first commit

---
+++ python.py
@@ -0,0 +1,36 @@
+# num = int(input("Enter you number from 1-10: "))
+
+# while num < 0 or num > 10:
+#     print("Out of range > 1-10")
+#     num = int(input("Enter you number from 1-10: "))
+
+# print(f"your number is {num}")
+
+# rows = int(input("Enter you number of rows: "))
+# cols = int(input("Enter you number of cols: "))
+# symbol = input("Enter shape of symbol you want: ")
+
+# for r in range(rows):
+#     for c in range(cols):
+#         print(symbol, end="")
+#     print()
+# for x in range(1, 10):
+#     if x == 5:
+#      break
+#     print(x, end=" ")
+# Generates an increasing triangle of stars
+# rows = 5
+# for i in range(1, rows + 1):
+#     for j in range(1, i + 1):
+#         print("*", end=" ")
+#     print()
+# Centers stars by printing leading spaces first
+# rows = 5
+# for i in range(1, rows + 1):
+#     print(" " * (rows - i) + "* " * i)
+for i in range(1, 10):
+    # if i % 2 == 1: find even
+        print(str((i)) + " supercalifragilisticexpialidocis")
+
+
+        
